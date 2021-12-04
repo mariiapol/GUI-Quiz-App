f86dbcb3181d6dd9f257c21d9374dbf9e3332f76
Mariia Polishchuk <94909216+mariiapol@users.noreply.github.com>
2021-12-04 14:03:10 +0000

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,6 @@ parameters = {
     "type": "boolean"
 }
 
-#https://opentdb.com/api.php?amount=10&type=boolean
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
